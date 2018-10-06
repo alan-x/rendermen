f3f743643e94a7ba72191962e55dfc82b1d5ccdc
FollowWinter <lyxxxx@yeah.net>
2018-10-06 15:35:52 +0800

    [更新] 修改注释, 删除没用的 escape 函数

--- src/renderermen.js
+++ src/renderermen.js
@@ -1,11 +1,9 @@
-function escape(html) {
-    return String(html)
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#039;');
-}
-
+/**
+ * 将模板转化成代码
+ *
+ * @param template 模板字符串
+ * @returns {string} 代码字符串
+ */
 function factory(template) {
     template = template
         .replace(/\n/g, '')
@@ -20,6 +18,13 @@ function factory(template) {
     return template
 }
 
+/**
+ * 将一个字符串渲染成模板函数
+ * 执行该行数返回真正的渲染后的模板字符串
+ *
+ * @param template 模板
+ * @returns {Function} 模板函数
+ */
 function render(template) {
     return new Function('model', factory(template))
 }
